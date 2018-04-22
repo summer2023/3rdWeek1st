2d1b066ced0b4ca14c7371d8ca8434af5df3707e
Shengzhi Wang <szwang@thoughtworks.com>
2018-04-22 15:58:44 +0800

    [shengzhi] some suggestions about InverseReduce，the answer is wrong.

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,7 +1,5 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,6 +16,11 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
+        /*
+        * 题目中是根据随机数生成的数来递减，不是写死的2
+        *  Integer divide = this.random.nextInt(3);
+        return IntStream.iterate(number-divide, item->item-divide).limit(number/divide).boxed().collect(Collectors.toList());
+        * */
         List<Integer> result = new ArrayList<Integer>();
         for (int i = number - 2; i > 0; i = i - 2) {
             result.add(i);
